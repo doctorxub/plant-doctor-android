f9273be9936530fd86a42a2f1f77f27cf5672df0
Alexi Akl <alexiakl@gmail.com>
2022-12-03 08:20:57 +0100

    feat: menu + capture image

--- app/src/main/java/com/doctorx/ui/MainActivity.kt
+++ app/src/main/java/com/doctorx/ui/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.doctorx.ui
 
 import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.ContentResolver
+import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.provider.OpenableColumns
+import android.view.MenuItem
 import android.view.View
+import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation.findNavController
@@ -15,10 +21,14 @@ import com.doctorx.databinding.ActivityMainBinding
 import com.doctorx.server.ApiInterface
 import com.doctorx.ui.diseases.DiseasesFragmentDirections
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import okhttp3.MediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
@@ -28,6 +38,7 @@ class MainActivity : AppCompatActivity() {
 
   companion object {
     const val REQUEST_CODE_PICK_IMAGE = 101
+    const val REQUEST_IMAGE_CAPTURE = 102
   }
 
   private var selectedImageUri: Uri? = null
@@ -43,8 +54,7 @@ class MainActivity : AppCompatActivity() {
     setContentView(binding.root)
 
     binding.fab.setOnClickListener { view ->
-//      showErrorMessage(view,"Replace with your own action")
-      openImageChooser()
+      showMenu(view)
     }
 
     controller = findNavController( binding.root.findViewById(R.id.nav_host_fragment_content_main))
@@ -59,6 +69,26 @@ class MainActivity : AppCompatActivity() {
     }
   }
 
+  fun showMenu(v: View) {
+    val popup = PopupMenu(this, v)
+    popup.inflate(R.menu.actions)
+    popup.setOnMenuItemClickListener(PopupMenu.OnMenuItemClickListener { item: MenuItem? ->
+
+      when (item!!.itemId) {
+        R.id.pickImage -> {
+          openImageChooser()
+        }
+        R.id.takePicture -> {
+          takePicture()
+        }
+      }
+
+      true
+    })
+
+    popup.show()
+  }
+
   override fun onResume() {
     super.onResume()
     controller.addOnDestinationChangedListener(listener)
@@ -70,11 +100,22 @@ class MainActivity : AppCompatActivity() {
   }
 
   private fun openImageChooser() {
-    Intent(Intent.ACTION_PICK).also {
+    Intent(Intent.ACTION_GET_CONTENT).also {
       it.type = "image/*"
       val mimeTypes = arrayOf("image/jpeg", "image/png")
       it.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
       startActivityForResult(it, REQUEST_CODE_PICK_IMAGE)
+    }
+  }
+
+  private fun takePicture() {
+    val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+    takePictureIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
+    takePictureIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+    try {
+      startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+    } catch (e: ActivityNotFoundException) {
+      // display error state to the user
     }
   }
 
@@ -127,11 +168,25 @@ class MainActivity : AppCompatActivity() {
       when (requestCode) {
         REQUEST_CODE_PICK_IMAGE -> {
           selectedImageUri = data?.data
-//          image_view.setImageURI(selectedImageUri)
+          uploadImage()
+        }
+        REQUEST_IMAGE_CAPTURE -> {
+          val photo = data?.extras?.get("data") as Bitmap
+          selectedImageUri = getImageUri(applicationContext, photo)
           uploadImage()
         }
       }
     }
+  }
+
+  fun getImageUri(inContext: Context, inImage: Bitmap): Uri? {
+    val bytes = ByteArrayOutputStream()
+    inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+    val path: String = MediaStore.Images.Media.insertImage(
+      inContext.getContentResolver(),
+      inImage, "Title", null
+    )
+    return Uri.parse(path)
   }
 
   fun getFileMultiPart( partName: String?,
